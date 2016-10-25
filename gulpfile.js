73b9062beb0a6f756f93fff37a894558b56a2619
guopeng <511734447@qq.com>
2016-10-25 14:32:24 +0800

    11

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const babel = require('gulp-babel');
 const LessPluginAutoPrefix = require('less-plugin-autoprefix');//css自动编译私有前缀
 const autoprefix = new LessPluginAutoPrefix({ browsers: ["ie >= 8", "ie_mob >= 10", "ff >= 26", "chrome >= 30", "safari >= 6", "opera >= 23", "ios >= 5", "android >= 2.3", "bb >= 10"] });
 
@@ -22,13 +23,16 @@ gulp.task('css',function(){
             path.extname = '.min.css'
         }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('public/dist/css'))
 });
 
 gulp.task('js',function(){
-    gulp.src('build/js/*.js')
+    gulp.src('build/es6/*.es')
         .pipe(sourcemaps.init({identityMap:true}))
         .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
         .pipe(uglify({compress:false}))
         .pipe(rename(function(path){
             path.extname = '.min.js'
@@ -39,7 +43,7 @@ gulp.task('js',function(){
 
 gulp.task('watch',function(){
     gulp.watch('build/less/*.less',['css']);
-    gulp.watch('build/js/*.js',['js']);
+    gulp.watch('build/es6/*.es',['js']);
 });
 
 gulp.task('default',['css','js','watch']);
