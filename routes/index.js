2111b39b659c4e4e8e678f0a772e5d3dfa53b94b
guopeng <511734447@qq.com>
2016-10-28 12:21:00 +0800

    111

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,13 @@ router.post('/upload',function(req,res){
   form.maxFieldsSize = 2 * 1024 * 1024;
   form.maxFields = 1000;
   form.multiples = true;
-
   form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.write('<script>');
+    res.write('window.parent.postMessage("123","*");');
+    res.end('</script>');
   });
-  res.send({ code: 'ok' });
+
 });
 
 module.exports = router;
